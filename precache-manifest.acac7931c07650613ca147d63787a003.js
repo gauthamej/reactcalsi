f2aa226ce041b753e000d423f7e122c954e5dbe9
Gautham Ej <you@example.com>
2020-03-12 13:20:31 +0530

    Updates

---
+++ precache-manifest.acac7931c07650613ca147d63787a003.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c384a60f35c0f8ade2ee65dbd77bd8",
+    "url": "/reactcalsi/index.html"
+  },
+  {
+    "revision": "ab503d9ef383c237c507",
+    "url": "/reactcalsi/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b9ad9cbe4058a26f24fa",
+    "url": "/reactcalsi/static/css/main.ddea0087.chunk.css"
+  },
+  {
+    "revision": "ab503d9ef383c237c507",
+    "url": "/reactcalsi/static/js/2.94e5daf0.chunk.js"
+  },
+  {
+    "revision": "98cef8f2c6f4a9493000de24ffc1680a",
+    "url": "/reactcalsi/static/js/2.94e5daf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ad9cbe4058a26f24fa",
+    "url": "/reactcalsi/static/js/main.ac0884f0.chunk.js"
+  },
+  {
+    "revision": "1ed4f8ae9e9dc33f312b",
+    "url": "/reactcalsi/static/js/runtime-main.527e8944.js"
+  }
+]);
